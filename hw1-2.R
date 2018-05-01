31bd7dc59603206161e6165268fecb79b467f12d
keiikegami <sybaster.x@gmail.com>
2018-05-02 08:19:09 +0900

    no message

--- hw1-2.R
+++ hw1-2.R
@@ -1,3 +1,4 @@
+# (2)
 log_diff <- ts(SA[["log"]])
 ma <- ts(SA[["MA"]])
 
@@ -20,3 +21,63 @@ for (i in 1:24){
 log_optimal_p <- which.min(AIC_log)
 
 # maの方が圧倒的にAICが小さい。のでmaの方がいいんじゃね？
+
+
+# (3)
+
+## (a)
+gap <- ts(official_gap[["gap"]])
+ts.plot(gap)
+
+## (b)
+install.packages("mFilter")
+library(mFilter)
+real_gdp <- read_csv("~/Desktop/2018summer/macro/hw1/empirics/real_gdp.csv")
+
+rgdp <- ts(real_gdp[["rgdp"]])
+hp_result <- hpfilter(rgdp)
+hp_trend <- hp_result$trend
+hp_cycle <- hp_result$cycle
+ts.plot(hp_cycle)
+
+## (c) unit root test
+install.packages("tseries")
+library(tseries)
+# ADF test
+result_adf <- adf.test(rgdp)
+# DF test
+result_df <- adf.test(rgdp, k = 0)
+### 棄却されないので、非定常であることがわかる。
+
+## (c)BN decomposition
+first_diff_rgdp <- diff(rgdp)
+# 差分系列のモデルとして最適なものを見つける。
+AIC_diff <- 1:5
+for (i in 1:5){
+  result <- arima(first_diff_rgdp, order = c(i,0,0))
+  AIC_diff[i] <- result$aic
+}
+
+diff_optimal_p <- which.min(AIC_diff)
+
+## これで選ばれるのが2なので、AR(2)における公式を使って出せる。
+## むしろAR(2)の時の問題をMorleyでちゃんととくのが大事
+rgdp_ts <- ts(real_gdp[["rgdp"]], c(1947,1),, 4)
+
+real_gdp[["X1"]][1]
+real_gdp[["X1"]][length(real_gdp[["X1"]])]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
